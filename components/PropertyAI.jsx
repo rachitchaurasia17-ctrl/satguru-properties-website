f458c68321f2adfafd37a37baaa0755e372c039c
rachitchaurasia17-ctrl <rachitchaurasia17@gmail.com>
2026-06-11 22:34:31 +0530

    Fix: replace all template addresses with client address

--- components/PropertyAI.jsx
+++ components/PropertyAI.jsx
@@ -11,7 +11,7 @@ const SYSTEM_PROMPT = `You are Property AI, the digital concierge for Satguru Pr
 
 Firm facts:
 - Firm: Satguru Properties
-- Office: SCO 1, 2nd Floor, Sector 79, Sahibzada Ajit Singh Nagar, Punjab 140308
+- Office: SCF 4, Near Satguru Complex, Baltana, Zirakpur, Punjab 140604
 - Phone: +91-00000-00000
 - GMADA empanelled, RERA registered (PB-RERA)
 - Practice: Residential, Commercial, Land/Plots, Investment Advisory, NRI Services, Title & Due Diligence
